5d3f13593a06ef34667a381934c7fae109f51e3f
DiNiSk5 <52897453+DiNiSk5@users.noreply.github.com>
2020-07-19 17:39:11 +1000

    Main file
    Where I will construct the interface for the LCD.

---
+++ user_interface/LCD/main.c
@@ -0,0 +1,27 @@
+#include "lpc24xx.h"
+#include "delay.h"
+#include "lcd/lcd_hw.h"
+#include "lcd/lcd_grph.h"
+#include "lcd/lcd_cfg.h"
+#include "lcd/sdram.h"
+
+int main(void) {
+	//Setup external SDRAM. Used for Frame Buffer
+	sdramInit();	
+	
+	//Setup LPC2478 LCD Controller for our specific LCD
+	//lcd_config is defined in lcd/lcd_cfg.h
+	lcdInit(&lcd_config); 
+	
+	//Turn the LCD on
+	lcdTurnOn();
+	
+	//Draw a background with lcd_fillScreen
+	//Otherwise you will write random noise to the screen!
+	
+	//Draw some shapes now!
+	//....
+
+	//Do nothing more
+	while (1) { }
+}
