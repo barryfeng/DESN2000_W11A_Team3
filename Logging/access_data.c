782772b8472dfe988925371dadd7ebece57b8f27
Adam Witt <z5264040@ad.unsw.edu.au>
2020-07-27 12:31:56 +0200

    Edit access_data by changing where Light_rail struct is stored

--- Logging/access_data.c
+++ Logging/access_data.c
@@ -12,22 +12,26 @@
  * -- 12/07/2020 --  File created.
  */
 
-#include "access_data.h"
+#include "master_controller.h"
+
+extern LightRail light_rail;
 
 extern void data_start(char *data_string, int SWI_operation);
 
-void store_data(Data data) {
-    int8_t int_velocity = data.velocity;
-    int8_t int_brake = data.brake;
+void store_data() {
+    uint32_t velocity = light_rail.velocity;
+    uint32_t vel_setpoint = light_rail.vel_setpoint;
+    uint8_t brake_state = light_rail.brake_state;
+    uint8_t dms_state = light_rail.dms_state;
 
     //String that contains all the data from the function arguments
     char data_string[64] = "";  //Size 64 is sufficient and also allows data to be aligned every two words
 
     char *velocity;
-    sprintf(velocity, "Velocity = %d km/h. ", int_velocity);
+    sprintf(velocity, "Velocity = %d km/h. ", velocity);
 
     char *brake;
-    sprintf(brake, "Brake state = %d. /n", int_brake);
+    sprintf(brake, "Brake state = %d. /n", brake);
 
     strcat(data_string, velocity); //Concatenate strings
     strcat(data_string, brake); //Concatenate strings
--- Logging/access_data.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <string.h>
-
-typedef struct data {
-    int8_t velocity;
-    int8_t brake;
-} Data;
