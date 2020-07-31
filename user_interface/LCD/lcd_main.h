354b5be94adafff2eee41cd408c5eae7da71b264
DiNiSk5 <52897453+DiNiSk5@users.noreply.github.com>
2020-08-01 00:02:04 +1000

    added new functions to header file

--- user_interface/LCD/lcd_main.h
+++ user_interface/LCD/lcd_main.h
@@ -14,7 +14,7 @@
 #define OFF 0
 #define ON 1
 #define RELEASE 0
-#define APPLY 0
+#define APPLY 1
 #define STOP 0
 #define MAX_VEL 50
 
@@ -22,20 +22,26 @@ void lcd_start();
 void lcd_run();
 
 void constant_borders();
+void constant_labels();
+
 void constant_velocity_on();
-void constant_throttle_on();
+void constant_throttle_plus_on();
+void constant_throttle_minus_on();
 void constant_stops_on();
 void constant_brake_on();
 void constant_dms_on();
 void constant_max_vel_on();
 void constant_sd_on();
+
 void constant_velocity_off();
-void constant_throttle_off();
+void constant_throttle_plus_off();
+void constant_throttle_minus_off();
 void constant_stops_off();
 void constant_brake_off();
 void constant_dms_off();
 void constant_max_vel_off();
 void constant_sd_off();
+
 void lcd_rectangle_thickness(unsigned short x0, unsigned short y0, unsigned short x1, unsigned short y1, int thickness, lcd_color_t color);
 void lcd_circle_thickness(unsigned short x0, unsigned short y0, unsigned short r, int thickness, lcd_color_t color);
 
