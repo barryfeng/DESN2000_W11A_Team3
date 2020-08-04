9e4aa4117468e49f8703baf662c7900160634533
Barry Feng <barry.feng.2001@hotmail.com>
2020-08-05 03:37:34 +1000

    compilation fixes

--- uvision/lcd_main.d
+++ uvision/lcd_main.d
@@ -14,3 +14,14 @@ lcd_main.o: C:\Keil\ARM\RV31\Inc\stdlib.h
 lcd_main.o: ..\..\DESN2000_W11A_Team3\master_controller.h
 lcd_main.o: ..\motion_control\../system/system_timer.h
 lcd_main.o: ..\motion_control\../system/system_init.h
+lcd_main.o: ..\..\DESN2000_W11A_Team3\user_interface/LCD/lcd_main.h
+lcd_main.o: ..\..\DESN2000_W11A_Team3\motion_control/motion_controller.h
+lcd_main.o: ..\..\DESN2000_W11A_Team3\user_interface/touchscreen/touch_screen_read.h
+lcd_main.o: ..\..\DESN2000_W11A_Team3\user_interface/touchscreen/button_press.h
+lcd_main.o: ..\..\DESN2000_W11A_Team3\user_interface/touchscreen/touch.h
+lcd_main.o: C:\Keil\ARM\RV31\Inc\stdio.h
+lcd_main.o: ..\..\DESN2000_W11A_Team3\logging/access_data.h
+lcd_main.o: ..\motion_control\fixed_point_pid.h
+lcd_main.o: ..\..\DESN2000_W11A_Team3\diagnostics/diagnostics.h
+lcd_main.o: ..\diagnostics\SPI_MMC.h
+lcd_main.o: ..\diagnostics\diagnostics.h
