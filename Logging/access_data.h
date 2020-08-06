a38af59ae1b3ebbd8188ee52e2efb75022831f48
Adam Witt <z5264040@ad.unsw.edu.au>
2020-08-06 15:03:37 +0200

    Commented access_data HEADER file

--- Logging/access_data.h
+++ Logging/access_data.h
@@ -3,6 +3,13 @@
 
 #include "master_controller.h"
 
+/*
+    The following functions are used to store data and to access
+    individual data items from memory. The data is velocity;
+    target velocity (set point); brake state; and DMS state
+    (dead man switch).
+*/
+
 void store_data(void);
 uint32_t access_velocity(void);
 uint32_t access_velocity_setpoint(void);
